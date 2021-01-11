4a7732e9a51f802147db763df9ec10248c3dcf6d
b08x <rwpannick@gmail.com>
2021-01-11 08:53:13 -0500

    refactoring search; finished backup script

--- backup.sh
+++ backup.sh
@@ -2,26 +2,17 @@
 
 MOUNT_STATUS=$(mount | grep bender)
 
-# sync
-if [ -d $MOUNT_STATUS ]
+SRC="/home/b08x/"
+DEST="/mnt/bender/b08x/"
+
+RSYNC="rsync -aiiP --recursive --verbose --force --stats --sparse"
+
+if [[ -d $MOUNT_STATUS ]]
   then
     echo "bender not mounted"
     exit
 fi
 
-rsync -avnz --partial --inplace --append --progress --recursive --stats \
---exclude-from=/home/b08x/.filter /home/b08x/ /mnt/bender/b08x/
-
-
-# click button in waybar
-# run rsync -n to show what files would be sent to bender
-# display diff
-# prompt; backup now?
+$RSYNC --exclude-from="$HOME/.filter" "$SRC" "$DEST" > "$HOME/backup.log" &
 
 # run rsync -n in the opposite direction
-# display diff
-# prompt; sync from bender?
-
-
-# workspace = home/workspace
-# workspace.children do {|folder in git repo| check status, prompt commit, push }
--- search.rb
+++ search.rb
@@ -8,7 +8,6 @@ require "shellwords"
 
 FOLDERS = ['archive', 'documents', 'workspace']
 
-#query = ARGV[0].shellescape
 
 class Zim
 
@@ -16,11 +15,20 @@ class Zim
 
   def initialize(query)
     @command = TTY::Command.new
-    @query = query
+    @query = query.shellsplit
   end
+
+  def assemble(terms)
+    last_word = terms.pop
+    terms.map {|term| term + " or"}.append(last_word)
+    return fer
+  end
+
 
   # @return [String]
   def self.search(fer)
+    fer = assemble(fer)
+
     results = @command.run("zim --search Notes '#{fer}'")
     return results
   end
@@ -31,55 +39,25 @@ class Zim
 
 end
 
-search_prompt = TTY::Prompt.new
 
-query = TTY::Prompt.new.ask("enter query", required: true, modify: :trim)
+# search phrase(formerly known as complete phrase)
+# phrase
+# query
+# results
+# term
+# subject = what
+#
 
-# split the query into an array of strings
-query = query.shellsplit
+module Query
+  module_function
 
-# append or operator to query.item
-phrase = lambda {|x| x + " or"}
+  def parse(subject)
+    terms = query.shellsplit
+    return terms
+  end
 
-# we don't want to add an operator after the last
-# so we pop it out of the array and assign it
-last_word = query.pop
+end
 
-# what will be the reassmebled query
-complete_phrase = []
+subject = TTY::Prompt.new.ask("What are you looking for?", required: true, modify: :trim)
 
-# for each word in the query, pass it through
-# the lambda function to append "or"
-query.each {|term| complete_phrase << phrase[term] }
-
-# after all that, add the last word
-complete_phrase.append(last_word)
-
-# take complete_phrase array and join
-# it back up as a string
-complete_phrase = complete_phrase.shelljoin
-
-# pass the phrase to the Zim search class
-results = Zim.search(complete_phrase)
-
-puts "#{results}"
-
-# put the pages found by Zim in array
-pages = results.out.split("\n")
-
-prompt = TTY::Prompt.new
-# prompt to select page(s) to open
-page = prompt.select("anything look good?", pages)
-# pass the selection to the Zim.open function
-Zim.open(page.shellescape)
-
-
-
-# set case insensitive, include hidden and be
-# more accurate than quick
-rga_options = ["-S", "--hidden", "--rga-accurate", "-e"]
-
-
-
-
-elvi = ["archwiki", "bugzilla","github","google", "stack"]
+terms = Query.parse(subject)
