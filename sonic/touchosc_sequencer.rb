d73dce7fa65599c2aac0a09ff5c50ac296e41664
b08x <rwpannick@gmail.com>
2021-07-05 06:58:01 -0400

    adding touchosc sequencer for sonic-pi. works.

---
+++ sonic/touchosc_sequencer.rb
@@ -0,0 +1,112 @@
+# Welcome to Sonic Pi
+
+in_thread(name: :clock) do
+  loop do
+    cue :clock
+    sleep 1
+  end
+end
+
+
+defonce :sequences do
+  # row 1
+  set :seq1, Array.new(16).map!{|x|x ?x:0}
+  # row 2
+  set :seq2, Array.new(16).map!{|x|x ?x:0}
+  # row 3
+  set :seq3, Array.new(16).map!{|x|x ?x:0}
+  # row 4
+  set :seq4, Array.new(16).map!{|x|x ?x:0}
+  # row 5
+  set :seq5, Array.new(16).map!{|x|x ?x:0}
+  # row 6
+  set :seq6, Array.new(16).map!{|x|x ?x:0}
+end
+
+def gen_seq_array(array,bool,index)
+  generate_sequence = lambda {|array,bool,index| array.delete_at(index);
+  array.insert(index,bool)}
+
+  generate_sequence.call(array,bool,index)
+end
+
+in_thread(name: :oscthing) do
+
+  live_loop :osc do
+    use_real_time
+    # this will be either 1 or 0
+    onoff = sync "/osc*/2/seq/*/*"
+
+    # this will be in a range from 1-6
+    row = parse_sync_address("/osc*/2/seq/*/*")[3].to_i
+    puts "row: #{row}"
+    # this will be in a range from 1-16
+    @index = parse_sync_address("/osc*/2/seq/*/*")[4].to_i
+
+    @index = (@index - 1)
+    print @index
+
+    @onoff = onoff.first
+
+    print @onoff
+
+    case row
+    when 1
+      puts "row1"
+      a = get[:seq1].dup
+      seqA = gen_seq_array(a,@onoff,@index)
+      set :seq1, seqA
+      print get[:seq1]
+    when 2
+      puts "row2"
+      a = get[:seq2].dup
+      seqA = gen_seq_array(a,@onoff,@index)
+      set :seq2, seqA
+      print get[:seq2]
+    when 3
+    when 4
+    when 5
+    when 6
+    end
+
+  end
+end
+
+use_synth :piano
+
+def note1
+  note = 52
+  return note
+end
+def note2
+  note = 66
+  return note
+end
+
+in_thread(name: :sound01) do
+
+  live_loop :first do
+    sync :clock
+
+    16.times do
+      tick
+      play note1, on: get[:seq1].ring.look
+      sleep 0.25
+    end
+    puts "end of seq1"
+
+  end
+
+  live_loop :second do
+    sync :clock
+
+    16.times do
+      tick
+      play note2, on: get[:seq2].ring.look
+      sleep 0.25
+    end
+    puts "end of seq2"
+  end
+
+
+end
